740f61e31c8340cf4dfae0cb782b001712aedbd8
carlos gonzalez <carlospayo17@gmail.com>
2016-05-17 15:42:01 +0200

    aa

--- success.php
+++ success.php
@@ -3,6 +3,6 @@
 echo '<h1>Operacion realizada con exito</h1>';
 echo '<h4>En unos segundos seras redirigido a la pagina principal</h4>';
 
-sleep(15);
-
+sleep(5);
+sleep(5);
 header("location: index.php");
